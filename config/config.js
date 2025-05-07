a22f35a624436ba401228358572f76b9cba0f6ef
Toby <latebloomerguy99@gmail.com>
2025-05-08 01:17:37 +0700

    fix config.js

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: "passwordsql123",
-    database: "ecommerce_shoes",
-    host: "localhost",
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "passwordsql123",
+    database: process.env.DB_NAME || "ecommerce_shoes",
+    host: process.env.DB_HOST || "localhost", // You need to update this to your Railway DB host
     dialect: "mysql",
     port: 3307,
   },
